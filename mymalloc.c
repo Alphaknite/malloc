e7ad4bf14eb2a86a0881b3c64bceba88eb531d61
Aditya Shah <reachadishah@gmail.com>
2025-02-18 12:22:46 -0500

    Started myfree

--- mymalloc.c
+++ mymalloc.c
@@ -136,7 +136,21 @@ void * mymalloc(size_t size, char *file, int line) {
 }
 
 void myfree(void *ptr, char *file, int line) {
+    if (ptr == NULL){
+        printf("Invalid pointer (%s:%d)\n", file, line);
+        return;
+    }
+    
+    MetaData *chunkToFree = (MetaData *)((char *)ptr - sizeof(MetaData));
 
+    if(chunkToFree->isFree == 1){
+        printf("Error: Freeing deallocated memory (%s:%d)\n", file, line);
+        return;
+    }
+    
+    if (chunkToFree->isFree == 0){
+        chunkToFree->isFree = 1;
+    }
 }
 
 
