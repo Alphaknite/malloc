d7f101ac0c0114ac9fa220ad22ac883413614c67
Rudra Patel <rudra5oct@gmail.com>
2025-02-25 15:47:48 -0500

    memgrind: test4 completed

--- memgrind.c
+++ memgrind.c
@@ -6,29 +6,29 @@
 
 void test1() {
     for(int i = 0; i < 120; i++) {
-        char *ptr = malloc(1);  
-        free(ptr);  
+        char *ptr = malloc(1);
+        free(ptr);
     }
 }
 
 void test2() {
-    char *ptrs[120]; 
+    char *ptrs[120];
 
     for(int i = 0; i < 120; i++) {
-        ptrs[i] = malloc(1); 
+        ptrs[i] = malloc(1);
         if(ptrs[i] == NULL) {
             fprintf(stderr, "malloc failed in test2\n");
         }
     }
 
     for(int i = 0; i < 120; i++) {
-        free(ptrs[i]);  
+        free(ptrs[i]);
     }
 }
 
 void test3() {
     char *ptrArray[120];
-    int allocated[120] = {0};  
+    int allocated[120] = {0};
     int index = 0;
 
     for(int i = 0; i < 120; i++) {
@@ -37,7 +37,8 @@ void test3() {
             ptrArray[index] = malloc(1);
             allocated[index] = 1;
             index++;
-        } else {
+        }
+        else {
             index--;
             printf("free index=%d\n", index);
             free(ptrArray[index]);
@@ -52,9 +53,51 @@ void test3() {
     }
 }
 
+typedef struct node {
+    char data;
+    struct node *next;
+} node_t;
+
+void test4() {
+    
+    node_t *head = NULL;
+
+    for(int i = 0; i < 120; i++) {
+        node_t *newNode = (node_t *)malloc(sizeof(node_t));
+        newNode->data = 'A' + i % 26;
+        newNode->next = head;
+        head = newNode;
+    }
+
+    // Print Linked List
+    printf("Linked List: ");
+    node_t *current = head;
+    while(current != NULL) {
+        printf("%c -> ", current->data);
+        current = current->next;
+    }
+    printf("NULL\n");
+
+    while(head) {
+        node_t *temp = head;
+        head = head->next;
+        free(temp);
+    }
+
+    // Print Linked List
+    printf("Linked List: ");
+    node_t *curr = head;
+    while(curr != NULL) {
+        printf("%c -> ", curr->data);
+        curr = curr->next;
+    }
+    printf("NULL\n");
+}
+
 int main() {
     test1();
     test2();
     test3();
+    test4();
     return EXIT_SUCCESS;
 }
