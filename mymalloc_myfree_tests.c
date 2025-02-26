23ec2acedf4e80c7ee6a73357bc6fc5d67a5ae90
Aditya Shah <reachadishah@gmail.com>
2025-02-25 21:17:01 -0500

    formatting in tests

--- mymalloc_myfree_tests.c
+++ mymalloc_myfree_tests.c
@@ -2,65 +2,84 @@
 #include <stdlib.h>
 #include <assert.h>
 
-#ifndef REALMALLOC
 #include "mymalloc.h"
-#endif
+
+#define MEMSIZE 4096
+#define HEADERSIZE 8
+
+void test_allocate_memory() {
+    void *ptr = malloc(300);
+    assert(ptr != NULL);
+    printf("Allocated memory sucessfully\n");
+    free(ptr);
+}
+
+void test_allocate_entire_heap() {
+    void *ptr = malloc(MEMSIZE - HEADERSIZE);
+    assert(ptr != NULL);
+    printf("Allocated the entire heap sucessfully\n");
+    free(ptr);
+}
+
+void test_allocate_too_much_memory() {
+    void *ptr = malloc(6000);
+    assert(ptr == NULL);
+    printf("Successfully rejected excessive memory request\n");
+}
+
+void test_allocate_zero_bytes() {
+    void *ptr = malloc(0);
+    assert(ptr == NULL);
+    printf("Successfully rejected request of zero bytes\n");
+}
 
 void test_coalesce() {
-    void *ptr1 = mymalloc(3500, __FILE__, __LINE__);
-    void *ptr2 = mymalloc(174, __FILE__, __LINE__);
-    void *ptr3 = mymalloc(390, __FILE__, __LINE__);
+    void *ptr1 = malloc(3500);
+    void *ptr2 = malloc(174);
+    void *ptr3 = malloc(390);
     
-    myfree(ptr2, __FILE__, __LINE__);
-    myfree(ptr3, __FILE__, __LINE__);
+    free(ptr2);
+    free(ptr3);
     
-    void *ptr4 = mymalloc(572, __FILE__, __LINE__);
+    void *ptr4 = malloc(572);
     assert(ptr4 != NULL);
 
-    printf("Coalesced and allocated new memory block\n");
+    printf("Sucessfully coalesced and allocated new memory block\n");
 
-    myfree(ptr1, __FILE__, __LINE__);
-    myfree(ptr4, __FILE__, __LINE__);
+    free(ptr1);
+    free(ptr4);
 }
 
 void test_myfree (int case_num) {
     switch (case_num) {
         case 1:
-            printf("Test freeing valid pointer:\n");
-            {
-                void *ptr = mymalloc(300, __FILE__, __LINE__);
-                myfree(ptr, __FILE__, __LINE__);
-            }
-            break;
-        
-        case 2:
             printf("Test freeing invalid pointer:\n");
             {
                 int num = 10;
-                void *invalid_ptr = &num;
-                myfree(invalid_ptr, __FILE__, __LINE__);
+                void *ptr = &num;
+                free(ptr);
+            }
+            break;
+
+        case 2:
+            printf("Test double free:\n");
+            {
+                void *ptr = malloc(240);
+                free(ptr);
+                free(ptr);
             }
             break;
 
         case 3:
-            printf("Test double free:\n");
-            {
-                void *ptr = mymalloc(240, __FILE__, __LINE__);
-                myfree(ptr, __FILE__, __LINE__);
-                myfree(ptr, __FILE__, __LINE__);
-            }
+            printf("Test freeing null pointer:\n");
+            free(NULL);
             break;
 
         case 4:
-            printf("Test freeing null pointer:\n");
-            myfree(NULL, __FILE__, __LINE__);
-            break;
-
-        case 5:
             printf("Test freeing a pointer that is not at the beginning of a chunk:\n");
             {
-                void *ptr = mymalloc(500, __FILE__, __LINE__);
-                myfree(ptr + 200, __FILE__, __LINE__);
+                void *ptr = malloc(500);
+                free(ptr + 200);
             }
             break;
 
@@ -68,15 +87,17 @@ void test_myfree (int case_num) {
             printf("Case number does not exist\n");
     }
 
-    printf("Only printing this statement if we free a valid pointer\n");
+    printf("Should not print this statement\n");
 }
 
 int main() {
+    test_allocate_memory();
+    test_allocate_entire_heap();
+    test_allocate_too_much_memory();
+    test_allocate_zero_bytes();
     test_coalesce();
+    test_myfree(1);
     test_myfree(2);
     test_myfree(3);
     test_myfree(4);
-    test_myfree(5);
 }
-
-
